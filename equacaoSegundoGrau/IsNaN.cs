b631d7f023c14880ad4d94f16d939f36d6b45ca2
jessicaconceicao <jessicacampos.dev@gmail.com>
2022-09-06 06:53:26 -0300

    Adicionar arquivos de projeto.

---
+++ equacaoSegundoGrau/IsNaN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace equacaoSegundoGrau
+{
+    public class IsNaN
+    {
+        public double valor;
+
+        public string VerifyNaN(double valor)
+        {
+            if (valor != valor)
+            { return "Número não existe"; }
+            else
+            { return valor.ToString("F"); }
+        }
+    }
+}
---
+++ equacaoSegundoGrau/Program.cs
@@ -0,0 +1,28 @@
+﻿//Dados valores de "a", "b" e "c", encontre as raízes da equação de segundo grau
+using equacaoSegundoGrau;
+
+int a, b, c;
+double delta, raizDeDelta, primeiraRaiz, segundaRaiz;
+
+Console.WriteLine("Digite o valor de a");
+int.TryParse(Console.ReadLine(), out a);
+
+Console.WriteLine("Digite o valor de b");
+int.TryParse(Console.ReadLine(), out b);
+
+Console.WriteLine("Digite o valor de c");
+int.TryParse(Console.ReadLine(), out c);
+
+delta = Math.Pow(b, 2) - (4 * a * c);
+
+raizDeDelta = Math.Sqrt(delta);
+
+primeiraRaiz = (-b + raizDeDelta) / (2 * a);
+segundaRaiz = (-b - raizDeDelta) / (2 * a);
+
+var verificaPrimeiraRaiz = new IsNaN();
+var verificaSegundaRaiz = new IsNaN();
+
+Console.WriteLine($"Primeira raiz: {verificaPrimeiraRaiz.VerifyNaN(primeiraRaiz)} | Segunda raiz: {verificaSegundaRaiz.VerifyNaN(segundaRaiz)};");
+
+
